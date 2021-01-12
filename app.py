af26712744b9d8cd8f55accfbe45700cb6e738bd
Julia-IO <tobetranslated01@gmail.com>
2021-01-12 18:13:22 +0000

    Add Add Linguist template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -24,6 +25,11 @@ def get_projects():
     print(os.environ.get("MONGO_URI"))
     projects = mongo.db.projects.find()
     return render_template("projects.html", projects=projects)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("add_linguist.html")
 
 
 if __name__ == "__main__":
