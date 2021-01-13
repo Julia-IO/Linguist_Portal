ca1e0dd0330f6023ee550397020520a04dc04d9d
Julia-IO <tobetranslated01@gmail.com>
2021-01-13 23:16:28 +0000

    Bind data from DB to collapsible

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_projects")
 def get_projects():
-    projects = mongo.db.projects.find()
+    projects = list(mongo.db.projects.find())
     return render_template("projects.html", projects=projects)
 
 
--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,6 @@ $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $('.tabs').tabs();
     $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
 
 });
