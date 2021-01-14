2249f06a8c76555db8fab9218717eb1d94d0f4a7
Julia-IO <tobetranslated01@gmail.com>
2021-01-14 18:12:11 +0000

    Wire up project edit button

--- app.py
+++ app.py
@@ -133,6 +133,12 @@ def add_project():
     status = mongo.db.status.find().sort("project_status", 1) # find all project status
     return render_template("add_project.html", categories=categories, users=users, leads=leads, status=status)
 
+@app.route("/edit_project/<project_id>", methods=["GET", "POST"])
+def edit_project(project_id):
+    project = mongo.db.projects.find_one({"_id": ObjectId(project_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("edit_project.html", project=project, categories=categories)
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
