38e6af8d5edb0bcd21a9f70a81eeb87ded93e585
Julia-IO <tobetranslated01@gmail.com>
2021-01-14 21:28:54 +0000

    Add Manage Categories templates and limit navbar access of certain menu items to admin

--- app.py
+++ app.py
@@ -171,6 +171,11 @@ def delete_project(project_id):
     flash("Project Successfully Deleted")
     return redirect(url_for("get_projects"))
 
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
